5d0735256b892bdac53154625ea5389afcae15a5
ybdn <baudrin.yoann@gmail.com>
2025-02-28 18:06:57 +0000

    Auto-update n8n workflows - Fri Feb 28 18:06:57 UTC 2025

--- sync_n8n.sh
+++ sync_n8n.sh
@@ -1,1 +1,11 @@
-git push origin master
+#!/bin/bash
+cd /var/lib/docker/volumes/n8n_data/_data/workflows
+
+# Vérifier s'il y a des modifications
+if [[ -n $(git status --porcelain) ]]; then
+    git add .
+    git commit -m "Auto-update n8n workflows - $(date)"
+    git push origin master
+else
+    echo "Aucune modification détectée, pas de push nécessaire."
+fi
